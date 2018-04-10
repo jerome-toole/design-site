3aab4bf58dffe0466dd3da2ff2e2795e6de67b9e
Jerome Toole <jerometoole@gmail.com>
2018-04-10 17:02:18 +0100

    change wording.

--- javascript/application.js
+++ javascript/application.js
@@ -6,13 +6,26 @@
 //   cssEase: 'linear'
 // });
 
+
+  //Defer image loading till rest of the page is loaded
+function init() {
+  var imgDefer = document.getElementsByTagName('img');
+  for (var i=0; i<imgDefer.length; i++) {
+    if(imgDefer[i].getAttribute('data-src')) {
+    imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+    } 
+  } 
+}
+window.onload = init;
+
 $('.post-images').each(function (idx, item) {
   var carouselId = "carousel" + idx;
   this.id = carouselId;
   $(this).slick({
     dots: true,
     infinite: true,
-    cssEase: 'linear',
+    cssEase: 'ease',
+    speed: 200,
     appendDots: $("#" + carouselId).closest('.browser').next('.dots-container')
   });
   $(this).click(function() {
